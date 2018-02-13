c2ea6719919802b394d66d5af025fa60c3223c6f
Glenn Hurd <glennqhurd@gmail.com>
2018-02-13 10:26:10 -0500

    Final commit, made style changes and finished functionality

--- Anagrams/Anagram.cs
+++ Anagrams/Anagram.cs
@@ -6,10 +6,45 @@ using System.Threading.Tasks;
 
 namespace Anagrams
 {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Anagram anagram = new Anagram();
+
+            // @"../../fileInput.txt" is the file name for the input file used.
+            anagram.ProcessFile(@"../../fileInput.txt");
+        }
+    }
+
+    class LoadFile
+    {
+        // I kept the load function in a separate class for clarity reasons and also so it could be reused
+        // if needed.  It reads a file and stores each line in a List element then returns the List<String>.
+        public List<String> ReadFile(String filename)
+        {
+            String line;
+            List<String> inputList = new List<String>();
+
+            // Read the file and display it line by line.  
+            System.IO.StreamReader file = new System.IO.StreamReader(filename);
+            while ((line = file.ReadLine()) != null)
+            {
+                inputList.Add(line);
+            }
+
+            file.Close();
+
+            return inputList;
+        }
+    }
+
     class Anagram
     {
         private Dictionary<String, List<String>> cValues = new Dictionary<String, List<String>>();
 
+        // Added a getter but no setter since I may want to retrieve the values but to change them I use
+        // separate methods.
         public Dictionary<String, List<String>> CheckedValues
         {
             get
@@ -18,6 +53,21 @@ namespace Anagrams
             }
         }
 
+        // Method that starts the chain of methods then after everything has been processed prints out 
+        // the results to Console
+        public void ProcessFile(String filename)
+        {
+            LoadFile lfile = new LoadFile();
+
+            ProcessList(lfile.ReadFile(filename));
+            List<String> greaterList = CountGreaterThanOne();
+            for (int i = 0; i < greaterList.Count; i++)
+            {
+                Console.WriteLine(greaterList[i]);
+            }
+        }
+
+        // Method that alphabetically sorts the String.
         private String SortString(String input)
         {
             char[] characters = input.ToArray();
@@ -25,6 +75,11 @@ namespace Anagrams
             return new string(characters);
         }
 
+        // Method that loops through a List<String> and uses SortString to sort each element then
+        // checks to see if the sorted element is already a key in the Dictionary CheckedValues.  If
+        // it is then the original unsorted element gets appended to the List<String> in the value.
+        // If there is not a key for the sorted element then a key is assigned and a new List<String> is
+        // added with an initial element added of the original value.
         private void ProcessList(List<String> input)
         {
             for(int i = 0; i < input.Count; i++)
@@ -41,10 +96,12 @@ namespace Anagrams
             }
         }
 
+        // Method that counts the number of values in the Dictionary for each key and appends each Key that has
+        // 2 or more elements in their List<String> value. 
         private List<String> CountGreaterThanOne()
         {
             Dictionary<String, List<String>>.KeyCollection keyColl = cValues.Keys;
-            List<String> result = new List<string>();
+            List<String> result = new List<String>();
 
             foreach (String s in keyColl)
             {
@@ -59,39 +116,6 @@ namespace Anagrams
                 }
             }
             return result;
-        }
-
-        public void ProcessFile(String filename)
-        {
-            String line;
-            List<String> inputList = new List<String>();
-
-            // Read the file and display it line by line.  
-            System.IO.StreamReader file =
-                new System.IO.StreamReader(filename);
-            while ((line = file.ReadLine()) != null)
-            {
-                inputList.Add(line);
-            }
-
-            file.Close();
-
-            ProcessList(inputList);
-            List<String> greaterList = CountGreaterThanOne();
-            for (int i = 0; i < greaterList.Count; i++)
-            {
-                Console.WriteLine(greaterList[i]);
-            }
-        }
-    }
-
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Anagram anagram = new Anagram();
-
-            anagram.ProcessFile(@"../../fileInput.txt");
         }
     }
 }
